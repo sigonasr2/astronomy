311da62018571646d130c7b3c9c8427bd21073cf
sigonasr2 <sigonasr2@gmail.com>
2020-07-09 11:11:24 -0500

    Include basic frontend, nodemon for backend server, and connectivity between all modules

--- server/app.js
+++ server/app.js
@@ -4,6 +4,15 @@ const port = 3005
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)) 
 const Pool = require('pg').Pool 
 
+  
+let allowCrossDomain = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', "*");
+	res.header('Access-Control-Allow-Headers', "*");
+	res.header('Access-Control-Allow-Methods', "*");
+	next();
+  }
+  app.use(allowCrossDomain);
+
 const pool = 
 new Pool({
   user: 'postgres',
@@ -14,7 +23,7 @@ new Pool({
 })
 
 const getUsers = (request, response) => {
-    pool.query('SELECT * FROM users', (error, results) => {
+    pool.query('SELECT * FROM signs', (error, results) => {
       if (error) {
 		response.status(500).json(error.message)
       } else {
