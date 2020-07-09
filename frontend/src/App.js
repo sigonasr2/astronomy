c22e7e1eccda6f350203617b7163de3e10ac114a
sigonasr2 <sigonasr2@gmail.com>
2020-07-09 14:08:44 -0500

    Completed introductory phase of Astronomy frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,34 @@ import React, {useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './fonts/ACROTSRG.TTF'
+import './backgrounds/stars.png'
+import DateTimePicker from 'react-datetime-picker'
 
 const REMOTE_ADDR = "http://45.33.13.215:3005";
 
 const axios = require('axios');
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return null;
+}
 
 const ClickSubmitButton = (input) => {
 	input.addEventListener("keydown", function(event) {
@@ -15,17 +39,73 @@ const ClickSubmitButton = (input) => {
 	});	
 }
 
+const ChooseFlavorLoadingText = () => {
+	var strings = [
+	"Communicating with the celestials...",
+	"Shooting for the stars...",
+	"Launching into the heavens...",
+	"Seeking heavenly advice...",
+	"Seeking divine wisdom...",
+	"Determining celestial intersection with the stars...",
+	"Communicating beyond mortal universes...",
+	"Heading into a cosmic universe...",
+	]
+	return strings[Math.floor(Math.random()*strings.length)]
+}
 
-const StartPage = ()=>{
+const UserPage = ()=>{
+	const [text,setText] = useState("");
+	const [page,setPage] = useState(null);
+	switch (page) {
+		default:{
+			return (<div className="container stars border rounded shadow-sm">
+				<div className="row">
+					<div className="col-md-12 header text-center">
+						<span className="glowHeavy animated fadein">{getCookie("username")}'s ASTRONOMY</span>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-12 text-center">
+						{text}
+					</div>
+				</div>
+			</div>);
+		}
+	}
+}
+
+
+const StartPage = (p)=>{
 	const [text,setText] = useState("");
 	const [loaded,setLoaded] = useState(null);
 	const [username,setUsername] = useState("");
+	const [date,setDate] = useState(new Date("Tue Jan 01 1991 00:00:00 GMT+0900"));
+	const [page,setPage] = useState(null);
+	
+	const SubmitNewUser = ()=>{
+		var obj = {username:username,birth_day:date.getDate(),
+		birth_month:date.getMonth(),birth_year:date.getFullYear(),
+		birth_minute:date.getMinutes(),birth_hour:date.getHours()}
+		console.log("Sending request:"+JSON.stringify(obj));
+		axios.post(REMOTE_ADDR+"/users/add/",obj)
+		.then((data)=>{
+			//Next create a cookie to remember this user's data.
+			console.log("Response: "+data);
+			for (var keys in obj) {
+				setCookie(keys,obj[keys],365);
+			}
+			//Now refresh the page...
+			p.setPage("UserPage");
+		})
+	}
+	
+	const SubmitBirthdate = (e)=>{
+		setPage("LOADING");
+		SubmitNewUser();
+	}
 	
 	const SubmitUsername = (e)=>{
-		setText(<>
-		<span className="slowblink">
-		Communicating with the celestials...
-		</span></>);
+		setPage("BIRTHDATE");
 	}
 	const UpdateUsername = (e)=>{
 		setUsername(e.currentTarget.value);
@@ -42,24 +122,80 @@ const StartPage = ()=>{
 	},12000)},[loaded])
 	useEffect(()=>{setTimeout(()=>{
 		setText(<><span className="fadein3">Tell me your name...</span>
+		<br/>
 		<input type="text" id="username" className="starbox" onChange={(e)=>{UpdateUsername(e)}}/>
+		<br/>
 		<button className="starbox" onClick={(e)=>SubmitUsername()} id="submit">Continue</button>
 		</>);
 		document.getElementById("username").focus();
 		ClickSubmitButton(document.getElementById("username"));
 	},14000)},[loaded])
-	return (<div className="container stars border rounded shadow-sm">
-		<div className="row">
-			<div className="col-md-12 header text-center">
-				<span className="glowHeavy animated fadein">ASTRONOMY</span>
-			</div>
-		</div>
-		<div className="row">
-			<div className="col-md-12 text-center">
-				{text}
-			</div>
-		</div>
-    </div>);
+	
+	switch (page) {
+		case "LOADING":{
+			return(
+				<div className="pb-3 container stars border rounded shadow-sm">
+					<div className="row">
+						<div className="col-md-12 header text-center">
+							<span className="glowHeavy animated fadein">ASTRONOMY</span>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-md-12 text-center">
+							<span className="slowblink">
+							{ChooseFlavorLoadingText()}
+							</span>
+						</div>
+					</div>
+				</div>
+			);
+		}break;
+		case "BIRTHDATE":{
+			return (
+				<div className="pb-3 container stars border rounded shadow-sm">
+					<div className="row">
+						<div className="col-md-12 header text-center">
+							<span className="glowHeavy animated fadein">ASTRONOMY</span>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-md-12 text-center">
+							<span className="fadein_2">
+							I also need to know when you were born. Don't be shy now... Tell it to me, relax.
+							<br/>
+							<DateTimePicker
+							  className="glowLight baseCalendar"
+							  autoFocus={true}
+							  onChange={(d)=>{setDate(d);
+							  console.log(d)}}
+								defaultView={"century"}
+							  value={date}
+							  clearIcon={null}
+							  disableClock={true}
+							/>
+							<br/>
+							<button className="starbox" onClick={(e)=>SubmitBirthdate()} id="submit">Continue</button>
+							</span>
+						</div>
+					</div>
+				</div>
+			);
+		}break;
+		default:{
+			return (<div className="pb-3 container stars border rounded shadow-sm">
+				<div className="row">
+					<div className="col-md-12 header text-center">
+						<span className="glowHeavy animated fadein">ASTRONOMY</span>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-12 text-center">
+						{text}
+					</div>
+				</div>
+			</div>);
+		}
+	}
 }
 
 function App() {
@@ -68,15 +204,34 @@ function App() {
 	const [page,setPage] = useState(null);
 
 	useEffect(()=>{
-		axios.get(REMOTE_ADDR)
-		.then((data)=>{
-			setData(data.data);
-			console.log(data);
-		})
+		if (getCookie("username")) {
+			setPage("UserPage");
+		} else {
+			setPage("StartPage");
+		}
 	},[loaded]);
 	switch (page) {
+		/*case "DateTimePicker":{
+			return <><DateTimePicker
+	      className="glowLight baseCalendar"
+		  autoFocus={true}
+          onChange={(date)=>{setDate(date);
+		  console.log(date)}}
+			defaultView={"century"}
+          value={date}
+		  clearIcon={null}
+		  disableClock={true}
+        />
+		</>
+		}*/
+		case "UserPage":{
+			return <UserPage/>;
+		}
+		case "StartPage":{
+			return <StartPage page={page} setPage={setPage}/>;
+		}break;
 		default:{
-			return <StartPage/>;
+			return <></>;
 		}
 	}
 }
--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express') 
 const app = express() 
 const port = 3005 
+const bodyParser = require('body-parser')
+const { json } = require('body-parser')
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)) 
 const Pool = require('pg').Pool 
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
   
 let allowCrossDomain = function(req, res, next) {
@@ -22,14 +30,132 @@ new Pool({
   port: 5432,
 })
 
-const getUsers = (request, response) => {
-    pool.query('SELECT * FROM signs', (error, results) => {
-      if (error) {
-		response.status(500).json(error.message)
-      } else {
-		response.status(200).json(results.rows)
+  const table_data = [{
+	  name:"users",
+	  required_fields:["username","birth_day","birth_month","birth_year","birth_minute","birth_hour"],
+	  all_fields:["username","birth_day","birth_month","birth_year","birth_minute","birth_hour"]
+  },{
+	  name:"signs",
+	  required_fields:[],
+	  all_fields:["sign_name","symbol","strengths","weaknesses","element","ruler","jewelry","yinyang"]
+  },{
+	  name:"compatibility",
+	  required_fields:[],
+	  all_fields:["signId","compatibleSignId"]
+  },{
+	  name:"numbers",
+	  required_fields:[],
+	  all_fields:["signId","compatibleSignId","compatible"]
+  },{
+	  name:"colors",
+	  required_fields:[],
+	  all_fields:["signId","color","compatible"]
+  },{
+	name:"flowers",
+	required_fields:[],
+	all_fields:["signId","flower","compatible"]
+  },{
+	name:"directions",
+	required_fields:[],
+	all_fields:["signId","direction","compatible"]
+  }
+];
+  
+  var RequiredFieldsExist = (body,fields) => {
+	  for (var keys of fields) {
+		if (!(keys in body)) {
+			return false;
+		}
 	  }
-    })
-}
-
-app.get('/', getUsers)
+	  return true;
+  }
+  
+  var OutputRequiredFieldNames = (required_fields) => required_fields.reduce((result,field)=>(result==="")?result+=field:result+=","+field,"");
+  var OutputSqlArgumentNumbers = (required_fields) => required_fields.reduce((result,field,count)=>(result==="")?result+="$"+(count+1):result+=","+"$"+(count+1),"");
+  var OutputBodyData = (body,required_fields) => required_fields.filter((field)=>body[field]!==undefined).map((field)=>body[field]);
+  var CountFieldNames = (body,all_fields) => {
+	var counter = 0;
+	for (var i=0;i<all_fields.length;i++) {
+		var field = all_fields[i];
+		if (body[field]) {
+			counter++;
+		}
+	}
+	return counter;
+  }
+  var OutputSqlArgumentsAndFieldNames = (body,all_fields) => {
+	var finalStr = "";
+	var count = 0;
+	for (var i=0;i<all_fields.length;i++) {
+		var field = all_fields[i];
+		if (body[field]) {
+			if (finalStr==="") {
+				finalStr += field+"=$"+(count+++1) 
+			} else {
+				finalStr += ","+field+"=$"+(count+++1) 
+			}
+		}
+	}
+	return finalStr;
+  }
+  
+  
+  
+  table_data.forEach((table)=>{
+	  app.get("/"+table.name+"/view",(req,res)=>{
+		  pool.query('SELECT * FROM '+table.name+' ORDER BY id ASC', (error, results) => {
+		  if (error) {
+			throw error
+		  }
+		  res.status(200).json(results.rows)
+		})
+	  });
+	  app.get("/"+table.name+"/view/:id",(req,res)=>{
+		  pool.query('SELECT * FROM '+table.name+' where id=$1 ORDER BY id ASC', [req.params.id] , (error, results) => {
+		  if (error) {
+			throw error
+		  }
+		  res.status(200).json(results.rows)
+		})
+	  });
+	  app.post("/"+table.name+"/add",
+		  (req,res)=>{
+	  if (req.body) { 
+		if (RequiredFieldsExist(req.body,table.required_fields)) {
+			  pool.query('insert into '+table.name+'('+OutputRequiredFieldNames(table.required_fields)+') values('+OutputSqlArgumentNumbers(table.required_fields)+') returning *', OutputBodyData(req.body,table.required_fields) , (error, results) => {
+			  if (error) {
+				throw error
+			  }
+			  res.status(200).json(results.rows)
+			})
+			} else {
+				res.status(400).json("Missing a field! Required Fields: "+OutputRequiredFieldNames(table.required_fields));
+			}
+	  }});
+	  app.put("/"+table.name+"/update/:id",
+		  (req,res)=>{
+		  if (req.body && req.params.id && CountFieldNames(req.body,table.all_fields)>0) { 
+		  //console.log([...OutputBodyData(req.body,table.all_fields),Number(req.params.id)])
+		  //console.log("update "+table.name+" set "+OutputSqlArgumentsAndFieldNames(req.body,table.all_fields)+" where id=$"+(Object.keys(req.body).length+1)+" returning *")
+			  pool.query("update "+table.name+" set "+OutputSqlArgumentsAndFieldNames(req.body,table.all_fields)+" where id=$"+(CountFieldNames(req.body,table.all_fields)+1)+" returning *", [...OutputBodyData(req.body,table.all_fields),req.params.id] , (error, results) => {
+				  if (error) {
+					throw error
+				  }
+				  res.status(200).json(results.rows)
+			})
+		  } else {
+			res.status(400).json("Missing id or invalid fields! Valid fields are: "+OutputRequiredFieldNames(table.all_fields));
+		  }});
+	  app.delete("/"+table.name+"/delete/:id",
+		  (req,res)=>{
+		  if (req.params.id) { 
+			  pool.query("delete from "+table.name+" where id=$1 returning *", [req.params.id] , (error, results) => {
+				  if (error) {
+					throw error
+				  }
+				  res.status(200).json(results.rows)
+			})
+		  } else {
+			res.status(400).json("Missing id!")
+		  }});
+  })
